99baa7e9b2cfa71cdc81d01cdb59102caa151df1
Nasif <nasiffrancis1@gmail.com>
2016-12-08 18:24:04 +0200

    made tests for UserAccount class

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -2,12 +2,12 @@
 from UserAccount import UserAccount
 
 #Three things are missing from the line below - fill them in
-my_user=UserAccount(  ,  ,  )
+my_user=UserAccount("sama","123","omg")
 
 #Call the print_secret method (function) - it takes one input - a guess for the password.
 
 #Use the wrong password as input here
-my_user.
+my_user.print_secret("321")
 
 #Use the right password here
-my_user.
+my_user.print_secret("123")
