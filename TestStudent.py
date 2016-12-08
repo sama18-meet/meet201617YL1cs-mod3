a6cc8f201e3e467d6c923fdccf844c72e8507ee4
Nasif <nasiffrancis1@gmail.com>
2016-12-08 19:55:01 +0200

    STILL NOT DONEgit add TestStudent.py!

---
+++ TestStudent.py
@@ -0,0 +1,12 @@
+from student import Student
+sama = Student("Sama", "Nazareth", "15", "162","chocolate")
+adi = Student("Adi", "Nazareth", "15.5", "150","vanilla")
+hala = Student("Hala", "Nazareth", "16", "155","banaa")
+
+sama.print_summary()
+adi.print_summary()
+hala.print_summary()
+
+sama.get_giraffe_gap()
+adi.get_giraffe_gap()
+hala.get_giraffe_gap()
